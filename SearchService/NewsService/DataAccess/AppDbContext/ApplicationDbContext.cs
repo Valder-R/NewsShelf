2ce063c148f09cc8d9a171eacb4d1225cfeaabce
LesiaKhaminova <lesiakhaminova@gmail.com>
2025-12-01 18:14:53 +0200

    Applied data access layer

---
+++ SearchService/NewsService/DataAccess/AppDbContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.AppDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<News> NewsItems => Set<News>();
+        public DbSet<NewsImage> NewsImages => Set<NewsImage>();
+    }
+}
---
+++ SearchService/NewsService/DataAccess/Entities/News.cs
@@ -0,0 +1,18 @@
+﻿
+namespace DataAccess.Entities
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public DateTime PublishedAt { get; set; } = DateTime.UtcNow;
+
+        public string? Author { get; set; }
+
+        public ICollection<NewsImage> Images { get; set; } = new List<NewsImage>();
+    }
+}
